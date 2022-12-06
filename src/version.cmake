82d9c02d621ecf727a41d1181114b923b9d8cc2b
Fred Emmott <git@fredemmott.com>
2022-12-06 08:35:49 -0600

    remove commit ID from file version string
    'local' is enough info

--- src/version.cmake
+++ src/version.cmake
@@ -1,36 +1,3 @@
-execute_process(
-  COMMAND git rev-parse --short HEAD
-  OUTPUT_VARIABLE COMMIT_ID
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-execute_process(
-  COMMAND git status --short
-  OUTPUT_VARIABLE MODIFIED_FILES
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-string(
-  REPLACE
-  "\n"
-  "\\n\\\n"
-  MODIFIED_FILES
-  "${MODIFIED_FILES}"
-)
-
-string(TIMESTAMP BUILD_TIMESTAMP UTC)
-
-execute_process(
-  COMMAND git log -1 "--format=%at"
-  OUTPUT_VARIABLE COMMIT_UNIX_TIMESTAMP
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if("${MODIFIED_FILES}" STREQUAL "")
-  set(DIRTY "false")
-else()
-  set(DIRTY "true")
-endif()
-
 # Need a tag first
 # execute_process(
 # COMMAND git describe --tags --abbrev=0 HEAD
@@ -43,7 +10,7 @@ set(LATEST_GIT_TAG "NO_TAGS")
 if("${VERSION_PATCH}" STREQUAL "0")
   string(REGEX MATCH "^v${VERSION_MAJOR}\\.${VERSION_MINOR}(\\.0|-|$)" MATCHING_TAG "${LATEST_GIT_TAG}")
 else()
-  string(REGEX MATCH "^v${VERSION_MAJOR}\\.${VERSION_MINOR}\\.${VERSION_PATCH}(\\.|-|$)" MATCHING_TAG "${LATEST_GIT_TAG}")
+  set(MATCHING_TAG OFF)
 endif()
 
 if(DEFINED ENV{GITHUB_RUN_NUMBER})
@@ -58,7 +25,7 @@ if("${RELEASE_NAME}" STREQUAL "")
   if(MATCHING_TAG)
     set(VERSION_SEMVER "${LATEST_GIT_TAG}+${BUILD_TYPE}.rev.${VERSION_BUILD}")
   else()
-    set(VERSION_SEMVER "v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}-alpha.rev.${VERSION_BUILD}+${BUILD_TYPE}")
+    set(VERSION_SEMVER "v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}+${BUILD_TYPE}")
   endif()
 endif()
 
