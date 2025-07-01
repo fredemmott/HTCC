1cdceee6955d3ac3d964ec514ea7eaf29ad34c37
Fred Emmott <git@fredemmott.com>
2025-07-01 12:38:23 -0500

    🔨 Add `usage` file to fredemmott-gui vcpkg port

--- third-party/vcpkg-overlay/fredemmott-gui/portfile.cmake
+++ third-party/vcpkg-overlay/fredemmott-gui/portfile.cmake
@@ -26,3 +26,4 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/third-party/microsoft-ui-xaml/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
