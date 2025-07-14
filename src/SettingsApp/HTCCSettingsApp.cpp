db3eaabfb4be6f8f36dcf764e5b111d6d12e0203
Fred Emmott <git@fredemmott.com>
2025-07-14 14:17:34 -0500

    ⬆️⚡ Update FUI
    Takes settings exe from 2.2mb to 912KB

--- src/SettingsApp/HTCCSettingsApp.cpp
+++ src/SettingsApp/HTCCSettingsApp.cpp
@@ -21,11 +21,11 @@
 #include "CheckHResult.hpp"
 #include "version.h"
 
+using namespace FredEmmott::GUI;
+using namespace FredEmmott::GUI::Immediate;
+
 namespace HTCC = HandTrackedCockpitClicking;
-namespace Config = HTCC::Config;
 namespace Version = HTCCSettings::Version;
-namespace fui = FredEmmott::GUI;
-namespace fuii = fui::Immediate;
 
 static HWND gWindowHandle {};
 
@@ -89,45 +89,42 @@ static bool IsAPILayerEnabled() noexcept {
 }
 
 void PointerSourceGUI() {
-  fuii::Caption("Hand tracking method");
-
   constexpr auto Options = std::array {
     "OpenXR hand tracking",
     "PointCTRL",
   };
-  static auto idx = static_cast<std::size_t>(Config::PointerSource);
-  if (!fuii::ComboBox(&idx, Options)) {
+  static auto idx = static_cast<std::size_t>(HTCC::Config::PointerSource);
+  if (!ComboBox(&idx, Options).Caption("Hand tracking method")) {
     return;
   }
 
-  Config::SavePointerSource(static_cast<HTCC::PointerSource>(idx));
+  HTCC::Config::SavePointerSource(static_cast<HTCC::PointerSource>(idx));
 }
 
 static bool& ShowUnsupportedSettings() {
   static bool sValue
-    = (Config::PointerSink == HTCC::PointerSink::VirtualVRController)
-    || !Config::UseHandTrackingAimPointFB;
+    = (HTCC::Config::PointerSink == HTCC::PointerSink::VirtualVRController)
+    || !HTCC::Config::UseHandTrackingAimPointFB;
   return sValue;
 }
 
 static void CommonSettingsGUI() {
-  fuii::BeginCard();
-  fuii::BeginVStackPanel();
+  BeginCard();
+  BeginVStackPanel();
 
-  fuii::Caption("Enable HTCC");
   static bool isEnabled = IsAPILayerEnabled();
-  if (fuii::ToggleSwitch(&isEnabled)) {
+  if (ToggleSwitch(&isEnabled).Caption("Enable HTCC")) {
     const DWORD disabled = isEnabled ? 0 : 1;
     wil::reg::set_value_dword(HKEY_LOCAL_MACHINE, APILayerSubkey, disabled);
   }
 
   PointerSourceGUI();
 
-  fuii::Caption("Show unsupported settings");
-  (void)fuii::ToggleSwitch(&ShowUnsupportedSettings());
+  (void)ToggleSwitch(&ShowUnsupportedSettings())
+    .Caption("Show unsupported settings");
 
-  fuii::EndVStackPanel();
-  fuii::EndCard();
+  EndVStackPanel();
+  EndCard();
 }
 
 static void UnsupportedSettingsGUI() {
@@ -135,69 +132,70 @@ static void UnsupportedSettingsGUI() {
     return;
   }
 
-  fuii::SubtitleLabel("Unsupported settings");
+  Label("Unsupported settings").Subtitle();
 
-  fuii::BeginCard();
-  fuii::BeginVStackPanel();
+  BeginCard();
+  BeginVStackPanel();
 
-  fuii::TextBlock(
+  TextBlock(
     "These settings can cause a worse experience, and are not recommended - "
     "turn them off if you encounter any issues.");
 
-  fuii::Caption("Emulate VR controllers in DCS World");
   static bool useControllersInDCS
-    = Config::PointerSink == HTCC::PointerSink::VirtualVRController;
-  if (fuii::ToggleSwitch(&useControllersInDCS)) {
+    = HTCC::Config::PointerSink == HTCC::PointerSink::VirtualVRController;
+  if (ToggleSwitch(&useControllersInDCS)
+        .Caption("Emulate VR controllers in DCS World")) {
     using enum HTCC::PointerSink;
-    Config::SavePointerSink(
+    HTCC::Config::SavePointerSink(
       useControllersInDCS ? VirtualVRController : VirtualTouchScreen);
   }
 
-  fuii::Caption("Ignore XR_FB_hand_tracking_aim pose");
-  bool ignoreAimPose = !Config::UseHandTrackingAimPointFB;
-  if (fuii::ToggleSwitch(&ignoreAimPose)) {
-    Config::SaveUseHandTrackingAimPointFB(!ignoreAimPose);
+  bool ignoreAimPose = !HTCC::Config::UseHandTrackingAimPointFB;
+  if (ToggleSwitch(&ignoreAimPose)
+        .Caption("Ignore XR_FB_hand_tracking_aim pose")) {
+    HTCC::Config::SaveUseHandTrackingAimPointFB(!ignoreAimPose);
   }
 
-  fuii::EndVStackPanel();
-  fuii::EndCard();
+  EndVStackPanel();
+  EndCard();
 }
 
 static void GesturesGUI() {
-  fuii::BeginEnabled(
-    Config::PointerSource == HTCC::PointerSource::OpenXRHandTracking);
-  fuii::SubtitleLabel("OpenXR hand tracking");
+  BeginEnabled(
+    HTCC::Config::PointerSource == HTCC::PointerSource::OpenXRHandTracking);
 
-  fuii::BeginCard();
-  fuii::BeginVStackPanel();
+  Label("OpenXR hand tracking").Subtitle();
 
-  fuii::Caption("Hold a hand up to suspend HTCC");
-  if (fuii::ToggleSwitch(&Config::HandTrackingHibernateGestureEnabled)) {
-    Config::SaveHandTrackingHibernateGestureEnabled();
+  BeginCard();
+  BeginVStackPanel();
+
+  if (ToggleSwitch(&HTCC::Config::HandTrackingHibernateGestureEnabled)
+        .Caption("Hold a hand up to suspend HTCC")) {
+    HTCC::Config::SaveHandTrackingHibernateGestureEnabled();
   }
 
-  fuii::Caption("Pinch to click");
-  if (fuii::ToggleSwitch(&Config::PinchToClick)) {
-    Config::SavePinchToClick();
+  if (ToggleSwitch(&HTCC::Config::PinchToClick).Caption("Pinch to click")) {
+    HTCC::Config::SavePinchToClick();
   }
 
-  fuii::Caption("Pinch to scroll");
-  if (fuii::ToggleSwitch(&Config::PinchToScroll)) {
-    Config::SavePinchToScroll();
+  if (ToggleSwitch(&HTCC::Config::PinchToScroll).Caption("Pinch to scroll")) {
+    HTCC::Config::SavePinchToScroll();
   }
 
-  fuii::EndVStackPanel();
-  fuii::EndCard();
-  fuii::EndEnabled();
+  EndVStackPanel();
+  EndCard();
+  EndEnabled();
 }
 
 static void PointCtrlCalibrationGUI() {
   static HTCC::PointCtrlSource sPointCtrl;
 
-  fuii::BeginEnabled(sPointCtrl.IsConnected());
-  fuii::BodyLabel("Calibration requires a PointCTRL device with HTCC firmware");
-  if (fuii::Button("Calibrate")) {
-    std::array<wchar_t, 32768> myPath;
+  BeginEnabled(sPointCtrl.IsConnected());
+
+  Label("Calibration requires a PointCTRL device with HTCC firmware").Body();
+
+  if (Button("Calibrate")) {
+    std::array<wchar_t, 32768> myPath {};
     const auto myPathLen
       = GetModuleFileNameW(nullptr, myPath.data(), myPath.size());
     ;
@@ -216,7 +214,7 @@ static void PointCtrlCalibrationGUI() {
       nullptr,
       SW_NORMAL);
   }
-  fuii::EndEnabled();
+  EndEnabled();
 }
 
 static void PointCtrlButtonMappingGUI() {
@@ -230,44 +228,42 @@ static void PointCtrlButtonMappingGUI() {
   auto count = Options.size();
   // Remove the deprecated option, unless it's selected
   if (
-    Config::PointCtrlFCUMapping
+    HTCC::Config::PointCtrlFCUMapping
     != HTCC::PointCtrlFCUMapping::DedicatedScrollButtons) {
     --count;
   }
 
-  static auto idx = static_cast<std::size_t>(Config::PointCtrlFCUMapping);
-  if (!fuii::ComboBox(&idx, std::views::take(Options, count))) {
+  static auto idx = static_cast<std::size_t>(HTCC::Config::PointCtrlFCUMapping);
+  if (!ComboBox(&idx, std::views::take(Options, count))) {
     return;
   }
 
-  Config::SavePointCtrlFCUMapping(static_cast<HTCC::PointCtrlFCUMapping>(idx));
+  HTCC::Config::SavePointCtrlFCUMapping(
+    static_cast<HTCC::PointCtrlFCUMapping>(idx));
 }
 
 static void PointCtrlGUI() {
-  fuii::BeginEnabled(Config::PointerSource == HTCC::PointerSource::PointCtrl);
-  fuii::SubtitleLabel("PointCTRL");
-  fuii::BeginCard();
-  fuii::BeginVStackPanel();
+  BeginEnabled(HTCC::Config::PointerSource == HTCC::PointerSource::PointCtrl);
+  Label("PointCTRL").Subtitle();
+  BeginCard();
+  BeginVStackPanel();
 
   PointCtrlCalibrationGUI();
   PointCtrlButtonMappingGUI();
 
-  fuii::EndVStackPanel();
-  fuii::EndCard();
-  fuii::EndEnabled();
+  EndVStackPanel();
+  EndCard();
+  EndEnabled();
 }
 
 static void AboutGUI() {
-  fuii::BeginHStackPanel();
-  fuii::Style({.mGap = 8});
+  BeginHStackPanel().Styled(Style().Gap(8));
 
-  fuii::FontIcon("\ue74c", fuii::FontIconSize::Subtitle);// OEM
-  fuii::Style({.mTranslateY = -4});
+  FontIcon("\ue74c", FontIconSize::Subtitle);
 
-  fuii::SubtitleLabel("About HTCC");
-  fuii::Style({.mFlexGrow = 1});
+  Label("About HTCC").Subtitle().Styled(Style().FlexGrow(1));
 
-  if (fuii::Button("Copy")) {
+  if (Button("Copy")) {
     if (OpenClipboard(gWindowHandle)) {
       const auto wide = HTCC::Utf8::ToWide(VersionString);
       wil::unique_hglobal buf(
@@ -280,25 +276,21 @@ static void AboutGUI() {
       CloseClipboard();
     }
   }
-  fuii::EndHStackPanel();
+  EndHStackPanel();
 
-  fuii::BeginCard();
-  fuii::TextBlock(VersionString);
-  fuii::EndCard();
+  BeginCard();
+  TextBlock(VersionString);
+  EndCard();
 }
 
 static void FrameTick() {
   using namespace FredEmmott::GUI::StaticTheme::Common;
-  fuii::BeginVScrollView();
-  fuii::Style({
-    .mBackgroundColor = LayerOnAcrylicFillColorDefaultBrush,
-  });
-  fuii::BeginVStackPanel();
-  fuii::Style({
-    .mGap = 12,
-    .mMargin = 12,
-    .mPadding = 8,
-  });
+  static const auto ScrollViewStyle
+    = Style().BackgroundColor(LayerOnAcrylicFillColorDefaultBrush);
+  BeginVScrollView().Styled(ScrollViewStyle);
+
+  static const auto LayoutStyle = Style().Gap(12).Margin(12).Padding(8);
+  BeginVStackPanel().Styled(LayoutStyle);
 
   CommonSettingsGUI();
   UnsupportedSettingsGUI();
@@ -306,8 +298,8 @@ static void FrameTick() {
   PointCtrlGUI();
   AboutGUI();
 
-  fuii::EndVStackPanel();
-  fuii::EndVScrollView();
+  EndVStackPanel();
+  EndVScrollView();
 }
 
 int WINAPI wWinMain(
@@ -315,17 +307,17 @@ int WINAPI wWinMain(
   [[maybe_unused]] HINSTANCE hPrevInstance,
   [[maybe_unused]] LPWSTR lpCmdLine,
   const int nCmdShow) {
-  return fui::Win32Window::WinMain(
+  return Win32Window::WinMain(
     hInstance,
     hPrevInstance,
     lpCmdLine,
     nCmdShow,
-    [](fui::Win32Window&) { FrameTick(); },
+    [](Win32Window&) { FrameTick(); },
     {"HTCC Settings"},
     {.mHooks = {
        .mBeforeMainLoop =
-         [](fui::Win32Window& window) {
-           Config::LoadBaseConfig();
+         [](Win32Window& window) {
+           HTCC::Config::LoadBaseConfig();
            gWindowHandle = window.GetNativeHandle();
          },
      }});
