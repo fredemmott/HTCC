b3c8688aa8fc45f59f662b97822473e372c0c213
Fred Emmott <git@fredemmott.com>
2026-02-03 15:20:19 -0600

    🐛 Fix missing includes in PointCtrlCalibration.cpp

--- src/PointCtrlCalibration/PointCtrlCalibration.cpp
+++ src/PointCtrlCalibration/PointCtrlCalibration.cpp
@@ -4,12 +4,14 @@
 #define XR_USE_PLATFORM_WIN32
 #define XR_USE_GRAPHICS_API_D3D11
 
+#include <Windows.h>
 #include <d2d1.h>
 #include <d3d11.h>
 #include <directxtk/SimpleMath.h>
 #include <dwrite.h>
 #include <openxr/openxr.h>
 #include <openxr/openxr_platform.h>
+#include <shellapi.h>
 
 #include <array>
 #include <chrono>
